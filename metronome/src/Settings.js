9e8fa7025f883d2f7b55354acebf206be6fc7765
kerinova <xeongs@hotmail.com>
2022-05-10 23:16:27 -0500

    added navigationn

---
+++ metronome/src/Settings.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './Settings.css';
+
+function Settings() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default Settings;
--- metronome/src/index.js
+++ metronome/src/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './Home'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Settings from './Settings'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>}>
+          <Route index element={<Home/>}/>
+          <Route path="settings" element={<Settings/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
